ff99936c9561cacb51bbd8aad9bcd117a0fa5a80
urantune <khangphucmn@outlook.com>
2025-12-08 21:14:20 +0700

    NEw

---
+++ src/main/java/webBackEnd/App.java
@@ -0,0 +1,19 @@
+package webBackEnd;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EntityScan(basePackages = "entities")
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class, args);
+
+
+
+    }
+
+}
