d771b52600776f382ce196a28aa4db90c239879c
Trần Đức Toàn <ToanTDCE191605@gmail.com>
2025-12-12 12:55:23 +0700

    new pic

--- src/main/java/webBackEnd/controller/Customer/HomeController.java
+++ src/main/java/webBackEnd/controller/Customer/HomeController.java
@@ -1,17 +1,23 @@
 package webBackEnd.controller.Customer;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import webBackEnd.entity.Customer;
 import webBackEnd.entity.Game;
+import webBackEnd.service.CustomerService;
 import webBackEnd.service.GameAccountService;
 
 import webBackEnd.service.GameService;
+import webBackEnd.service.WalletService;
 import webBackEnd.successfullyDat.PathCheck;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping(value = "/home")
@@ -22,9 +28,15 @@ public class HomeController {
     private GameAccountService gameAccountService;
 
     @Autowired
+    private CustomerService customerService;
+
+    @Autowired
     private PathCheck pathCheck;
     @Autowired
     private GameService gameService;
+
+    @Autowired
+    private WalletService walletService;
 
     @GetMapping
     public String home(Model model) {
@@ -32,9 +44,16 @@ public class HomeController {
 //            System.out.println(pathCheck.getPathWithOS(gameDetailService.getGameDetailByGameAccount(a).getMainImage()));
 //        }
         model.addAttribute("list20Product", gameAccountService.get20Profuct());
+
         List<Game> game = gameService.findAllGame();
         model.addAttribute("game", game);
         return "customer/index";
     }
+
+    @GetMapping("/news")
+    public String news(Model model) {
+        return "customer/news";
+    }
+
 
 }
--- src/main/java/webBackEnd/repository/WalletRepositories.java
+++ src/main/java/webBackEnd/repository/WalletRepositories.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import webBackEnd.entity.Wallet;
 
+import java.util.UUID;
+
 @Repository
 public interface WalletRepositories extends JpaRepository<Wallet,Integer> {
+
 }
