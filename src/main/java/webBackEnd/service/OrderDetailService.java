3a63d3abe56fce526524a28d0df47f869fcc2f6b
urantune <khangphucmn@outlook.com>
2025-12-17 16:47:31 +0700

    changepass

--- src/main/java/webBackEnd/service/OrderDetailService.java
+++ src/main/java/webBackEnd/service/OrderDetailService.java
@@ -8,6 +8,7 @@ import webBackEnd.entity.OrderDetail;
 import webBackEnd.entity.Orders;
 import webBackEnd.repository.OrderDetailRepositories;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,13 +17,25 @@ public class OrderDetailService {
 
     @Autowired
     private OrderDetailRepositories orderDetailRepositories;
+    @Autowired
+    private OrdersService  ordersService;
     public List<GameAccount> getAllBoughtAccounts(UUID customerId) {
         return orderDetailRepositories.findAllGameAccountBoughtByCustomer(customerId);
     }
 
 
     public List<OrderDetail> findAllByOrderId(UUID orderId) {
-        return orderDetailRepositories.findAllByOrderId(orderId);
+        return orderDetailRepositories.findAllByOrder(ordersService.findById(orderId));
+    }
+
+
+    public List<OrderDetail> getOrderDetailByOrder(Orders o){
+        return orderDetailRepositories.findByOrder(o);
+    }
+
+    public OrderDetail getFirstOrderDetailByOrder(Orders o) {
+        List<OrderDetail> list = getOrderDetailByOrder(o);
+        return list.isEmpty() ? null : list.get(0);
     }
 
     public OrderDetail findById(UUID id) {
@@ -33,9 +46,7 @@ public class OrderDetailService {
         orderDetailRepositories.delete(orderDetail);
     }
 
-    public OrderDetail getOrderDetailByOrder(Orders o){
-        return orderDetailRepositories.findByOrder(o);
-    }
+
 
 }
 
